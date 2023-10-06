d17f5c10834c93fde9356592a3159fdbe582f02c
tomaspa-dev <blord.dev@gmail.com>
2023-10-06 17:38:58 -0500

    Data Types
    Excercise with data types

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Tipos de Datos / Data Types
+//Primitivos y Objeto / Primitives and Objects
+
+let variable = "Hello World";
+console.log(
+  `1 - Soy la variable - ${variable} , del tipo - ${typeof variable}`
+);
+variable = true;
+console.log(
+  `2 - Soy la variable - ${variable} , del tipo - ${typeof variable}`
+);
+variable = 12563;
+console.log(
+  `2 - Soy la variable - ${variable} , del tipo - ${typeof variable}`
+);
