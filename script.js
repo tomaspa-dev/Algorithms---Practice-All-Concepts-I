f52102569a80cb6509f629dceac9c649c825855a
tomaspa-dev <blord.dev@gmail.com>
2023-10-08 13:37:07 -0500

    Nullish coalescing assignment
    excercise

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 "use strict";
 
+// //1 Excercises
+
+let a = null;
+a ??= "valor predeterminado";
+console.log(a);
+
+// const a = 1;
+// const result = a == true ? true : false;
+// console.log(result);
+
+// let myMap = new Map();
+// myMap.set("foo", { name: "baz", desc: "inga" });
+
+// let nameBar = myMap.get("faa")?.name;
+// console.log(nameBar);
+
+// //1 Excercises
+// let a = true;
+// let b = 0;
+// let c = 23;
+
+// a &&= 10;
+// console.log(a);
+
+// //1 Excercises
+
+// //0) Excercises
+// const var1 = 10;
+// const var2 = 1;
+// let result = 10;
+
+// if (var1 > 19) {
+//   result &&= var1;
+// } else {
+//   result ||= var2;
+// }
+
+// console.log(result);
+// //0) Excercises
+
 //Tipos de Datos / Data Types
 //Primitivos y Objeto / Primitives and Objects
 
@@ -15,32 +55,35 @@
 // console.log(
 //   `2 - Soy la variable - ${variable} , del tipo - ${typeof variable}`
 // );
+//Primitivos y Objeto / Primitives and Objects
 
-const entries = new Map([
-  ["foo", "bar"],
-  ["baz", 42],
-]);
+////No primitivos - Objects
+// const entries = new Map([
+//   ["foo", "bar"],
+//   ["baz", 42],
+// ]);
 
-const obj = Object.fromEntries(entries);
+// const obj = Object.fromEntries(entries);
 
-console.log(obj, typeof obj);
-// Expected output: Object { foo: "bar", baz: 42 }
+// console.log(obj, typeof obj);
+// // Expected output: Object { foo: "bar", baz: 42 }
 
-let newObjet1 = {};
-let newObjet2 = new Object();
-let newObjet3 = { name: "MyName" };
-const persona = { nombre: "Juan", edad: 30 }; //Object literal
-function Persona(nombre, edad) {
-  this.nombre = nombre;
-  this.edad = edad;
-}
-const juan = new Persona("Juan", 30);
+// let newObjet1 = {};
+// let newObjet2 = new Object();
+// let newObjet3 = { name: "MyName" };
+// const persona = { nombre: "Juan", edad: 30 }; //Object literal
+// function Persona(nombre, edad) {
+//   this.nombre = nombre;
+//   this.edad = edad;
+// }
+// const juan = new Persona("Juan", 30);
 
-class Personas {
-  constructor(nombre, edad) {
-    this.nombre = nombre;
-    this.edad = edad;
-  }
-}
-const pedro = new Personas("Pedro", 30);
-console.log(pedro);
+// class Personas {
+//   constructor(nombre, edad) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//   }
+// }
+// const pedro = new Personas("Pedro", 30);
+// console.log(pedro);
+////No primitivos - Objects
